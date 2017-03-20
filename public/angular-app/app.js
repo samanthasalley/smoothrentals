232d656c3d828005e2318b426d04fa6f3b7037fb
Samantha Salley <samanthasalley@gmail.com>
2017-03-20 10:41:59 -0700

    Minor updates

--- public/angular-app/app.js
+++ public/angular-app/app.js
@@ -5,6 +5,30 @@ function config($routeProvider){
         .when('/', {
             templateUrl: 'angular-app/components/home/home.html'
         })
+        .when('/listings', {
+            templateUrl: 'angular-app/components/listings/listings.html'
+        })
+        .when('/listings/:listId', {
+            templateUrl: 'angular-app/components/listings/item-display.html'
+        })
+        .when('/listings/:listId/apply', {
+            templateUrl: 'angular-app/components/applications/apply.html'
+        })
+        .when('/listings/:listId/apply/:appId', {
+            templateUrl: 'angular-app/components/applications/application-display.html'
+        })
+        .when('/listings/:listId/comment/:commentId', {
+            templateUrl: 'angular-app/components/comments/comment-display.html'
+        })
+        .when('/login', {
+            templateUrl: 'angular-app/components/users/login.html'
+        })
+        .when('/register', {
+            templateUrl: 'angular-app/components/users/register.html'
+        })
+        .when('/profile', {
+            templateUrl: 'angular-app/components/users/profile-display.html'
+        })
         .otherwise({
             redirectTo: '/'
         });
--- public/angular-app/shared/main/app-controller.js
+++ public/angular-app/shared/main/app-controller.js
@@ -4,8 +4,8 @@ function AppController($scope, $location){
     
     $scope.title = 'Smooth Rentals';
 
-    // $scope.isActiveTab = function(url){
-    //     var currentPath = $location.path().split('/')[1];
-    //     return (url === currentPath ? 'active' : '');
-    // };
+    $scope.isActiveTab = function(url){
+        var currentPath = $location.path().split('/')[1];
+        return (url === currentPath ? 'active' : '');
+    };
 }

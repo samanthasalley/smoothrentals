10bc771a44bd53c1b922f337898a97b140156909
Samantha Salley <samanthasalley@gmail.com>
2017-04-10 00:31:51 -0700

    Add listing get route controllers and add listing controller

--- api/controllers/listings.controllers.js
+++ api/controllers/listings.controllers.js
@@ -5,14 +5,86 @@ var mongoose    = require("mongoose"),
 
 module.exports.listingsGetAll = function(req, res) {
     console.log('getting all listings');
+    Listing
+    	.find()
+    	.exec(function(err, listings){
+    		if(err){
+    			res
+    				.status(500)
+    				.json(err);
+    		}else{
+    			res
+    				.status(200)
+    				.json(listings);
+    		}
+    	});
 };
 
 module.exports.listingsAddOne = function(req, res) {
     console.log('adding one listing');
+    var newListing = {
+        address1: req.body.address1,
+	    address2: req.body.address2 || null,
+	    city: req.body.city,
+	    state: req.body.state,
+	    country: req.body.country,
+	    zip: req.body.zip,
+	    rent: req.body.rent,
+	    deposit: req.body.deposit,
+	    leaseTerm: req.body.leaseTerm,
+	    utilitiesIncl: req.body.utilitiesIncl,
+	    dateAvailable: req.body.dateAvailable,
+	    squareFt: req.body.squareFt || null,
+	    pets: req.body.pets || null,
+	    bedrooms: req.body.bedrooms,
+	    bathrooms: req.body.bathrooms,
+	    parking: req.body.parking || null,
+	    amenities: req.body.amenities || null,
+	    furnished: req.body.furnished || null,
+	    description: req.body.description || null
+    };
+    
+    Listing
+        .create(newListing, function(err, listing){
+            var response = {
+                status : 201,
+                message : {}
+            };
+            if(err){
+                response.status = 500;
+                response.message = err;
+            } else {
+                response.message = listing;
+            }
+            res
+                .status(response.status)
+                .json(response.message);
+        });
 };
 
 module.exports.listingsGetOne = function(req, res) {
     console.log('getting one listing');
+    var listId = req.params.listId;
+    Listing
+    	.findById(listId)
+    	.exec(function(err, listing){
+            var response = {
+                status : 200,
+                message : {}
+            };
+            if(err){
+                response.status = 500;
+                response.message = err;
+            }else if(!listing){
+                response.status = 404;
+                response.message = "Listing ID not found";
+            }else{
+            	response.message = listing;
+            }
+            res
+                .status(response.status)
+                .json(response.message);
+        });
 };
 
 module.exports.listingsUpdateOne = function(req, res) {

e1bdd6607402d94ba4c4928e4e30d24ab3e49ce3
Samantha Salley <samanthasalley@gmail.com>
2017-04-17 14:49:50 -0700

    Change email to not require unique value

--- api/data/users.model.js
+++ api/data/users.model.js
@@ -3,7 +3,6 @@ var mongoose = require("mongoose");
 var userSchema = new mongoose.Schema({
     email: {
         type: String,
-        unique: true,
         required: true
     },
     username: {

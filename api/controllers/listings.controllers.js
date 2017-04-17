50eaef6018a5207805553e9d461a6abbe8af7a6e
Samantha Salley <samanthasalley@gmail.com>
2017-04-17 16:10:50 -0700

    Add working listingsUpdateOne function

--- api/controllers/listings.controllers.js
+++ api/controllers/listings.controllers.js
@@ -89,6 +89,61 @@ module.exports.listingsGetOne = function(req, res) {
 
 module.exports.listingsUpdateOne = function(req, res) {
     console.log('updating one listing');
+    var listId = req.params.listId;
+
+    Listing
+        .findById(listId)
+        .select("-listor -applications -comments")
+        .exec(function(err, listing){
+            var response = {
+                status : 200,
+                message : listing || null
+            };
+            if(err){
+                response.status = 500;
+                response.message = err;
+            } else if(!listing){
+                response.status = 404;
+                response.message = "Listing ID not found";
+            }
+            if(response.status !== 200){
+                res
+                    .status(response.status)
+                    .json(response.message);
+            } else {
+                listing.address1 = req.body.address1;
+                listing.address2 = req.body.address2 || null;
+                listing.city = req.body.city;
+                listing.state = req.body.state;
+                listing.country = req.body.country;
+                listing.zip = req.body.zip;
+                listing.rent = req.body.rent;
+                listing.deposit = req.body.deposit;
+                listing.leaseTerm = req.body.leaseTerm;
+                listing.utilitiesIncl = req.body.utilitiesIncl;
+                listing.dateAvailable = req.body.dateAvailable;
+                listing.squareFt = req.body.squareFt || null;
+                listing.pets = req.body.pets || null;
+                listing.bedrooms = req.body.bedrooms;
+                listing.bathrooms = req.body.bathrooms;
+                listing.parking = req.body.parking || null;
+                listing.amenities = req.body.amenities || null;
+                listing.furnished = req.body.furnished || null;
+                listing.description = req.body.description || null;
+                
+                listing.save(function(err, updatedListing){
+                    if(err){
+                        res
+                            .status(500)
+                            .json(err);
+                    }else{
+                        res
+                            .status(204)
+                            .json({success: true, listing: updatedListing});
+                    }
+                });
+            }
+        });
 };
 
 module.exports.listingsDeleteOne = function(req, res) {
